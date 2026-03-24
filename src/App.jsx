9898d3a634d71a89965a9679741e90360f754b5c
laura <lauras14159@gmail.com>
2026-03-24 13:06:08 +0200

    add seo

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,46 @@ import { Experience } from "./components/Experience";
 import { Contact } from "./components/Contact";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
+import { Helmet } from "react-helmet-async";
 
 function App() {
   return (
-    <div className="min-h-screen overflow-hidden">
-      <Navbar />
-      <main>
-        <Hero />
-        <Skills />
-        <Projects />
-        <Experience />
-        <Contact />
-      </main>
-    </div>
+    <>
+      <Helmet>
+        <title>Laura K – Frontend Developer | React & Tailwind Portfolio</title>
+        <meta
+          name="description"
+          content="Explore Laura K's React & Tailwind portfolio showcasing interactive web projects and a passion for creative work, including beginner 3D modeling with Blender. View projects, contact for collaboration."
+        />
+        <meta
+          property="og:title"
+          content="Laura K – Frontend Developer | React & Tailwind Portfolio"
+        />
+        <meta
+          property="og:description"
+          content="Explore Laura K's React & Tailwind portfolio showcasing interactive web projects and a passion for creative work, including beginner 3D modeling with Blender. View projects, contact for collaboration."
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://portfolio-nine-woad-32.vercel.app/"
+        />
+        <meta
+          property="og:image"
+          content="https://portfolio-nine-woad-32.vercel.app/preview.png"
+        />
+      </Helmet>
+      <div className="min-h-screen overflow-hidden">
+        <Navbar />
+        <main>
+          <Hero />
+          <Skills />
+          <Projects />
+          <Experience />
+          <Contact />
+        </main>
+      </div>
+    </>
   );
 }
 
